951894165e476b191f82b2958630d2505c1514bb
Aarush Chhiber <aarush_chhiber@Aarushs-MacBook-Pro-2.local>
2024-09-15 16:37:49 -0400

    Restaurant View

--- food_finder/views.py
+++ food_finder/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import Restaurant
+from django.http import JsonResponse
 # Create your views here.
+
+def restaurant_data(request):
+    restaurants = Restaurant.objects.all()
+
+    restaurant_list = [
+        {
+        "name" : restaurant.name,
+        "lat" : restaurant.latitude,
+        "long" : restaurant.longitude,
+        "info" : restaurant.detais
+    }
+    for restaurant in restaurants
+    ]
+
+    return JsonResponse(restaurant_list, safe=False)
