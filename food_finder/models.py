2d75be1e4f266793993425389bdadf83d11e117a
Sanshubh Kukutla <kukutla.sanshubh@gmail.com>
2024-09-15 16:39:38 -0400

    added food finder app files

--- food_finder/models.py
+++ food_finder/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    cuisine_type = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    distance = models.FloatField()
+
+    def __str__(self):
+        return self.name
--- food_finder/views.py
+++ food_finder/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Restaurant
+from .forms import RestaurantSearchForm
 
-# Create your views here.
+def search_restaurants(request):
+    form = RestaurantSearchForm(request.GET or None)
+    restaurants = Restaurant.objects.all()
+
+    if form.is_valid():
+        if form.cleaned_data['name']:
+            restaurants = restaurants.filter(name__icontains=form.cleaned_data['name'])
+        if form.cleaned_data['cuisine_type']:
+            restaurants = restaurants.filter(cuisine_type__icontains=form.cleaned_data['cuisine_type'])
+        if form.cleaned_data['location']:
+            restaurants = restaurants.filter(location__icontains=form.cleaned_data['location'])
+        if form.cleaned_data['min_rating']:
+            restaurants = restaurants.filter(rating__gte=form.cleaned_data['min_rating'])
+        if form.cleaned_data['max_distance']:
+            restaurants = restaurants.filter(distance__lte=form.cleaned_data['max_distance'])
+
+    context = {
+        'form': form,
+        'restaurants': restaurants
+    }
+
+    return render(request, 'restaurant/search_results.html', context)
