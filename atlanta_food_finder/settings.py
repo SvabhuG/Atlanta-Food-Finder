40a075ba335dc9c8ff7e87e014990331ca9213a4
SvabhuGovindaraj <svabhug@amazon.com>
2024-09-30 16:18:33 -0400

    Removed unnecessary files

--- atlanta_food_finder/settings.py
+++ atlanta_food_finder/settings.py
@@ -11,16 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-<<<<<<< HEAD
 import os
-=======
 import environ
 
 # Initialize environment variables
 env = environ.Env()
 environ.Env.read_env()
-
->>>>>>> main
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
